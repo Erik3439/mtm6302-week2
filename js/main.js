141688c2852d00230c56ac314408f56d677067d7
Erik John <john1412@algonquinlive.com>
2023-05-26 12:19:41 -0400

    changes

---
+++ js/main.js
@@ -0,0 +1,49 @@
+//console.log("js file included")
+/* the above line of code is commented out using a single 
+line comment, the shortcut is control *
+
+this comment is a multiline comment, shortcut is shift * alt *  A
+*/
+let myName = "Erik"
+console.log(myName)
+
+myName = "Erik John"
+console.log(myName)
+
+const address = "Earth"
+console.log("address")
+
+let myProffession
+console.log(myProffession)
+
+// array is an object data type    
+const myScore =[56, 76, 98]
+console.log(myScore)
+
+let num = 10
+num += 2
+console.log(num)
+
+console.log(num== 12)
+console.log(num > 90)
+console.log(!(num !==90))
+
+console.log(num >= 7 && num !== 12)
+
+console.log(num >= 7 || num !== 12)
+ 
+console.log(num.toFixed(2)) 
+
+numA = '1'
+numB = 1
+console.log(numA + numB)
+
+
+console.log(parseInt(numA) + numB)
+
+const pets = 2
+const pet1name = "simba"
+const pet2name = "bufi"
+
+console.log(" i have " + pets + " dogs " + pet1name + " & " + pet2name)
+console.log(` I have ${pets} dogs, ${pet1Name} & ${pet2Name}`)  
